c0fd768d0d5a0758b3d22a4f8ab95116744546d1
linjun <1045735402@qq.com>
2017-12-28 21:14:05 +0800

    添加事件处理

--- src/main/java/com/linjun/config/WeixinConfig.java
+++ src/main/java/com/linjun/config/WeixinConfig.java
@@ -3,14 +3,14 @@ package com.linjun.config;
 import java.util.StringTokenizer;
 
 public class WeixinConfig {
-    public  static  final  String WX_APPID="wx3d391035e237c6bc";
-    public  static  final  String WX_APPSECRET="15c1963c192fe4e9be18735422ab8cab";
-    public  static  final String REDIRECT_URI="http://linjunjj.free.ngrok.cc/wechat";
+    public  static  final  String WX_APPID="wxe0cd32449ecbbc02";
+    public  static  final  String WX_APPSECRET="e013ff9369099a3ef3888b3ef9b9c38b";
+    public  static  final String REDIRECT_URI="http://www.shunnonghui.com/wechat";
     public  static  final  String MCH_ID="1493683362";
     public  static  final  String APPID="wxe0cd32449ecbbc02";
     public static final String WXUNIFIEDORDER_HTTPURL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
    public  static final  String API_KEY = "CErjCXMqdC5nXoYRIWI1YDz8B4UUv65m";
-    public  static  final  String KeyPath="/Users/linjun/Downloads/cert/apiclient_cert.p12";
-    public  static  final String notifyUrl="http://linjunjj.free.ngrok.cc";
+    public  static  final  String KeyPath="/www/backend/apiclient_cert.p12";
+    public  static  final String notifyUrl="http://www.shunnonghui.com/payresult/result/";
      public  static final String BAIDULation="3vmb6zina9IM6ku0mkMONj8ge62Gj030";
 }
