ed4b5ecbfb8d402430709f73dc43cf5dcd547b4a
linjun <1045735402@qq.com>
2018-01-24 22:34:31 +0800

    添加事件处理

--- src/main/java/com/linjun/controller/AdminController.java
+++ src/main/java/com/linjun/controller/AdminController.java
@@ -258,43 +258,6 @@ public JsonResult login(@RequestParam(value = "id",required = false)long id
     }
 
 
-    private Long id;
-
-    private String nickname;
-
-    private String userheadimg;
-
-    private String password;
-
-    private String sex;
-
-    private Byte userrole;
-
-    private Date birthday;
-
-    private Long tel;
-
-    private Byte userstutal;
-
-    private String address;
-
-    private Integer infofull;
-
-    private String email;
-
-    private String school;
-
-    private String major;
-
-    private Long parisesum;
-
-    private Date createtime;
-
-    private Date logouttime;
-
-    private Long logoutip;
-    private  long  fens;
-    private  long awardSum;
 
 
 
